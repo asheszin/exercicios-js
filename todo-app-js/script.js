8f43ac7b831831cd411cb3653d2d7493fe5eb59a
Lucas <163414962+asheszin@users.noreply.github.com>
2024-03-15 18:48:37 -0300

    add salvarTarefas()
    Agora as tarefas são salvas em localstorage

--- todo-app-js/script.js
+++ todo-app-js/script.js
@@ -28,6 +28,7 @@ function criaTarefa(tarefa) {
     tarefas.appendChild(li)
     limpaInput()
     botaoApagar(li)
+    salvarTarefa()
 }
 
 function botaoApagar(li){
@@ -43,5 +44,30 @@ document.addEventListener('click', function(e){
 
     if(el.classList.contains('apagar')){
         el.parentElement.remove()
+        salvarTarefa()
     }
 })
+
+function salvarTarefa(){
+    const liTarefas = tarefas.querySelectorAll('li')
+    const listaDeTarefas = []
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim()
+        listaDeTarefas.push(tarefaTexto)
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas)
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionarTarefasSalvas(){
+    const tarefas = localStorage.getItem('tarefas')
+    const listaDeTarefas = JSON.parse(tarefas)
+
+    for(let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa)
+    }
+}
+adicionarTarefasSalvas()
