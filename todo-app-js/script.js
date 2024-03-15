7d4a5d4fb79eaf34b7381be6840285af2c323617
Lucas <163414962+asheszin@users.noreply.github.com>
2024-03-15 18:27:50 -0300

    add botão remover tarefa

--- todo-app-js/script.js
+++ todo-app-js/script.js
@@ -1,6 +1,7 @@
 const inputTarefa = document.querySelector('.input-tarefa')
 const btnTarefa = document.querySelector('.btn-tarefa')
 const tarefas = document.querySelector('.tarefas')
+const apagarBtn = document.querySelector('.apagar')
 
 btnTarefa.addEventListener('click', function(e){
     if(!inputTarefa.value) return;
@@ -26,4 +27,21 @@ function criaTarefa(tarefa) {
     li.innerText = tarefa
     tarefas.appendChild(li)
     limpaInput()
+    botaoApagar(li)
 }
+
+function botaoApagar(li){
+    li.innerText += ' '
+    const botaoApagar = document.createElement('button')
+    botaoApagar.innerText = 'Apagar'
+    botaoApagar.setAttribute('class', 'apagar')
+    li.appendChild(botaoApagar)
+}
+
+document.addEventListener('click', function(e){
+    const el = e.target
+
+    if(el.classList.contains('apagar')){
+        el.parentElement.remove()
+    }
+})
