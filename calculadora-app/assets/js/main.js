773ba6d7f8e26e1ddc55432e015008d343cbf7fb
Lucas <163414962+asheszin@users.noreply.github.com>
2024-03-19 16:54:24 -0300

    Atualizado
    Utilizando arrow functions e constructor function

--- calculadora-app/assets/js/main.js
+++ calculadora-app/assets/js/main.js
@@ -1,74 +1,47 @@
-function criaCalculadora(){
-    return {
-        display: document.querySelector('.display'),
-        btnClear: document.querySelector('.btn-clear'),
+function Calculadora() {
+    this.display = document.querySelector('.display')
 
-        inicia(){
-           this.cliqueBotoes()
-           this.pressionaEnter()
-        },
-        
-        clearDisplay() {
-            this.display.value = ''
-        },
+    this.capturaCliques = () => {
+        document.addEventListener('click', e => {
+            const el = e.target
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el)
+            if (el.classList.contains('btn-clear')) this.clear()
+            if (el.classList.contains('btn-del')) this.del()
+            if (el.classList.contains('btn-eq')) this.realizaConta()
+        })
+    }
+    this.inicia = () => {
+        this.capturaCliques()
+        this.capturaEnter()
+    }
+    this.capturaEnter = () => {
+        document.addEventListener('keypress', e => {
+            if (e.keyCode !== 13) return
+            this.realizaConta()
+        })
+    }
 
-        pressionaEnter() {
-            document.addEventListener('keydown', (e) => {
-                if (e.keyCode === 13) {
-                    e.preventDefault()
-                    console.log('Tecla Enter pressionada');
-                    this.realizaConta()
-                }
-            })
-        },
-        realizaConta() {
-            let conta = this.display.value;
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText
+        this.display.focus()
+    }
+    this.clear = () => this.display.value = ''
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value)
 
-            try {
-                conta = eval(conta)
-
-                if(!conta) {
-                    alert('Conta invalida');
-                    return;
-                }
-                this.display.value = conta
-            } catch(e) {
-                alert('Conta invalida');
-                return;
+            if(!conta) {
+                alert('Conta inválida')
+                return
             }
-        },
-
-        apagaUm() {
-            this.display.value = this.display.value.slice(0, -1)
-        },
-
-
-
-        cliqueBotoes(){
-            document.addEventListener('click', function(e){
-                const el = e.target
-                
-                if(el.classList.contains('btn-num')) {
-                    this.btnParaDisplay(el.innerText);
-                }
-                if(el.classList.contains('btn-clear')) {
-                    this.clearDisplay()
-                }
-                if(el.classList.contains('btn-del')) {
-                    this.apagaUm()
-                }
-                if(el.classList.contains('btn-eq')) {
-                    this.realizaConta()
-                }
-            }.bind(this))        
-        },
-
-        btnParaDisplay(valor) {
-            this.display.value += valor;
+            this.display.value = conta
+        } catch(e) {
+            alert('Conta inválida')
+            return
         }
-    };
+    }
 }
 
-const calculadora = criaCalculadora();
-
-calculadora.inicia()
+const calculadora = new Calculadora();
+calculadora.inicia();
